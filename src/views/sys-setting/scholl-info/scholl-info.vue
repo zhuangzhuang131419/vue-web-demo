c199c9906a407ffac0c4638921f5a804bd13c303
zhengchicheng.bob <zhengchicheng.bob@bytedance.com>
2020-06-15 14:38:47 +0800

    修改数据字段

--- src/views/sys-setting/scholl-info/scholl-info.vue
+++ src/views/sys-setting/scholl-info/scholl-info.vue
@@ -4,9 +4,9 @@
         <el-form-item label="渠道名称：">
           <el-input size="small" v-model="formInline.channelName" placeholder="渠道名称" @input="change($event)"></el-input>
         </el-form-item>
-        <el-form-item label="渠道ID：">
-          <el-input size="small" v-model="formInline.channelID" placeholder="渠道ID"></el-input>
-        </el-form-item>
+<!--        <el-form-item label="渠道ID：">-->
+<!--          <el-input size="small" v-model="formInline.channelID" placeholder="渠道ID"></el-input>-->
+<!--        </el-form-item>-->
 <!--        <el-form-item label="管理员：">-->
 <!--          <el-input size="small" v-model="formInline.manager" placeholder="管理员"></el-input>-->
 <!--        </el-form-item>-->
@@ -18,13 +18,15 @@
 
       <el-table :data="schoolShowList" size="small" style="width: 100%;">
         <el-table-column label="ID"  width="80" prop="id"></el-table-column>
-        <el-table-column label="渠道名称" width="180" prop="channel_name"></el-table-column>
-        <el-table-column label="渠道ID"  width="120" prop="channel_id"></el-table-column>
-        <el-table-column label="研发"  width="180" prop="development"></el-table-column>
-        <el-table-column label="产品"  width="100" prop="product"></el-table-column>
-        <el-table-column label="法务"  width="100" prop="law"></el-table-column>
-        <el-table-column label="创建时间"  width="300" prop="create_at"></el-table-column>
-        <el-table-column label="上次更改时间"  width="300" prop="last_update_at"></el-table-column>
+        <el-table-column label="渠道名称" width="100" prop="channel_name"></el-table-column>
+        <el-table-column label="产品"  width="400" prop="product"></el-table-column>
+        <el-table-column label="HQ Country"  width="100" prop="HQ_country"></el-table-column>
+        <el-table-column label="覆盖国家"  width="100" prop="country"></el-table-column>
+        <el-table-column label="是否可转售"  width="80" prop="resalable" :formatter="formatBool"></el-table-column>
+        <el-table-column label="支付方式"  width="100" prop="payment_type"></el-table-column>
+        <el-table-column label="备注"  width="300" prop="description"></el-table-column>
+        <el-table-column label="创建时间"  width="200" prop="create_at"></el-table-column>
+        <el-table-column label="上次更改时间"  width="200" prop="last_update_at"></el-table-column>
         <el-table-column fixed="right" label="操作" width="280">
           <template slot-scope="scope">
             <el-button @click.native.prevent="toEditRow(scope.row)" type="primary" size="small">编辑</el-button>
@@ -43,25 +45,40 @@
       </div>
 
       <div class="dialog">
-        <el-dialog title="渠道信息" :inline="true" :visible.sync="modalVisible" width="30%" :before-close="closeAddModal">
-         <el-form ref="form" :model="form" label-width="100px" size="mini" class="demo-dynamic">
+        <el-dialog title="渠道信息" :inline="true" :visible.sync="modalVisible" width="40%" :before-close="closeAddModal">
+         <el-form ref="form" :model="form" label-width="120px" size="mini" class="demo-dynamic">
             <el-form-item label="渠道名称：" prop="channel_name"
-              :rules="[{ required: true, message: '请输入渠道名称', trigger: 'blur' }]">
+              :rules="[{ required: true, message: '请输入渠道名称', trigger: 'blur' }]" :disabled="!isAdd">
               <el-input v-model="form.channel_name"></el-input>
             </el-form-item>
-            <el-form-item label="渠道ID：" prop="channel_id" 
-              :rules="[{ required: true, message: '请输入渠道ID', trigger: 'blur' }]">
-             <el-input v-model="form.channel_id"></el-input>
+            <el-form-item label="HQ Country：" prop="HQ_country" 
+              :rules="[{ required: true, message: '请输入渠道总部国家', trigger: 'blur' }]">
+             <el-input v-model="form.HQ_country"></el-input>
             </el-form-item>
-            <el-form-item label="产品：">
-             <el-input v-model="form.product"></el-input>
-            </el-form-item>
-            <el-form-item label="研发：">
-             <el-input v-model="form.development"></el-input>
-            </el-form-item>
-            <el-form-item label="法务：">
-             <el-input v-model="form.law"></el-input>
-            </el-form-item>
+           <el-form-item label="Country：" prop="country" 
+             :rules="[{ required: true, message: '请输入渠道覆盖国家', trigger: 'blur' }]">
+             <el-input v-model="form.country"></el-input>
+           </el-form-item>
+           <el-form-item label="是否可转售：" prop="resalable"
+              :rules="[{ required: true, message: '是否可以转售', trigger: 'blur' }]">
+             <el-radio-group v-model="form.resalable">
+               <el-radio :label=true>是</el-radio>
+               <el-radio :label=false>否</el-radio>
+             </el-radio-group>
+           </el-form-item>
+           <el-form-item label="支付方式：" prop="payment_type">
+             <el-radio-group v-model="form.payment_type">
+               <el-radio v-for="payment_type in paymentTypes" :label="payment_type" :key="payment_type">{{payment_type}}</el-radio>
+             </el-radio-group>
+           </el-form-item>
+           
+           <el-form-item label="产品：">
+             <el-input type="textarea" autosize v-model="form.product" placeholder="请输入产品内容"></el-input>
+           </el-form-item>
+           <el-form-item label="备注：">
+             <el-input type="textarea" autosize v-model="form.description" placeholder="请输入备注内容"></el-input>
+           </el-form-item>
+
           </el-form>
           <span slot="footer" class="dialog-footer">
             <el-button @click="closeAddModal">取 消</el-button>
@@ -80,15 +97,16 @@ export default {
   data () {
     return {
       formInline: {
-        channelName: '',
-        channelID: ''
+        channelName: ''
       },
       schoolList: [],
       currentPage: 1,
       pageSize: 10,
       modalVisible: false,
       form: {},
-      isAdd: false
+      // 是处于添加状态还是编辑状态
+      isAdd: false,
+      paymentTypes: ['Cards', 'Cash', 'Wallet', 'DCB', 'Points', 'BankTransfer', 'DirectDebit', 'GiftCards', 'AppStore']
     };
   },
   created () {
@@ -109,28 +127,33 @@ export default {
     toAddRow () {
       this.form = {
         'id': '',
-        'channel_id': '',
         'channel_name': '',
-        'development': '',
+        'HQ_country': '',
+        'country': '',
         'product': '',
-        'law': '',
+        'resalable': '',
+        'payment_type': '',
         'create_at': '',
-        'last_update_at': ''
+        'last_update_at': '',
+        'description': ''
       };
       this.modalVisible = true;
       this.isAdd = true;
     },
     addRow () {
-      if (this.form.channel_id === '' || this.form.channel_name === '') {
-        alert('请输入渠道名称和渠道ID');
+      if (this.form.channel_name === '') {
+        alert('请输入渠道名称');
         return;
       }
+
       var para = {
         'channel_name': this.form.channel_name,
-        'channel_id': this.form.channel_id,
+        'HQ_country': this.form.HQ_country,
+        'country': this.form.country,
         'product': this.form.product,
-        'development': this.form.development,
-        'law': this.form.law
+        'resalable': this.form.resalable,
+        'payment_type': this.form.payment_type,
+        'description': this.form.description
       };
 
       api.insert(JSON.stringify(para));
@@ -138,8 +161,7 @@ export default {
       this.modalVisible = false;
     },
     toEditRow (row) {
-      console.log('编辑');
-      console.log(row);
+      console.log('编辑' + row);
       this.form = row;
       console.log('ID:' + this.form.id);
       this.modalVisible = true;
@@ -171,17 +193,22 @@ export default {
       api.deleteBDQRecord(rowId);
     },
     _editRow () {
-      var para = {
+      const para = {
         'id': this.form.id,
         'channel_name': this.form.channel_name,
-        'channel_id': this.form.channel_id,
+        'HQ_country': this.form.HQ_country,
+        'country': this.form.country,
         'product': this.form.product,
-        'development': this.form.development,
-        'law': this.form.law
+        'resalable': this.form.resalable,
+        'payment_type': this.form.payment_type,
+        'description': this.form.description
       };
 
       api.updateBDQRecord(JSON.stringify(para));
       this.modalVisible = false;
+    },
+    formatBool: function (row, column) {
+      return row.resalable ? '是' : '否';
     }
   },
   computed: {
@@ -191,8 +218,7 @@ export default {
     schoolShowList () {
       let ths = this;
       let returnList = ths.schoolList.filter(function (item) {
-        return (ths.formInline.channelID === '' || item.channel_id.indexOf(ths.formInline.channelID) !== -1) &&
-          (ths.formInline.channelName === '' || item.channel_name.indexOf(ths.formInline.channelName) !== -1);
+        return (ths.formInline.channelName === '' || item.channel_name.indexOf(ths.formInline.channelName) !== -1);
       });
 
       return returnList && returnList.slice((ths.currentPage - 1) * ths.pageSize, ths.currentPage * ths.pageSize);
